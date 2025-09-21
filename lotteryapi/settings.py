a5c531c95adb1ca39d3223d499c2a989cd9fe281
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2025-09-22 00:02:45 +0530

    static

--- lotteryapi/settings.py
+++ lotteryapi/settings.py
@@ -144,3 +144,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
