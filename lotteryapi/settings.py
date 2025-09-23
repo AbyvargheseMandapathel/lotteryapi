219ab71a7e0cf116488796b16489cf9476b48dbc
Aby varghese <56151184+AbyvargheseMandapathel@users.noreply.github.com>
2025-09-23 15:11:39 +0530

    Update settings.py
    Added akhil domain

--- lotteryapi/settings.py
+++ lotteryapi/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-3f@j1^$y_=z1yq*f=e(s#sm=_1=rox=amp&)!qyx$khjezn+0#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["69.62.77.40","127.0.0.1"]
+ALLOWED_HOSTS = ["69.62.77.40","127.0.0.1","keralalotteryresults.vercel.app"]
 
 
 
